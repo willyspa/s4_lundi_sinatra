0b7eadc0c1cb4f117b30d5eef4e922ae00f66b7d
willyspa <armed95@hotmail.fr>
2019-01-28 12:32:57 +0100

    au secours cynthia

--- controller.rb
+++ controller.rb
@@ -1,10 +1,13 @@
 require 'bundler'
 Bundler.require
+require 'rack'
+require 'shotgun'
 
 class ApplicationController < Sinatra::Base
+
   get '/' do
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index
   end
 
-  run! if app_file == $0
+
 end
